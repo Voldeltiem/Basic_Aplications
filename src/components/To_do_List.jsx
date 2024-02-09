1183d3563d3131a276eeb3078ab48a608b1c7d6a
Voldeltiem <arielstancovichsuarez@gmail.com>
2024-02-09 16:28:25 -0300

    se termina mecanica de creacion y eliminacion de tareas

--- src/components/To_do_List.jsx
+++ src/components/To_do_List.jsx
@@ -1,30 +1,49 @@
-import React, { useState } from 'react'
-import { Context } from 'react'
-import { useContext } from 'react'
-import Contexto from '../Context/Context'
+import React, { useState, useContext } from 'react';
+import Contexto from '../Context/Context';
 
 export default function To_do_List() {
-    const {tareas, setTareas} = useContext(Contexto)
-    const {tarea , setTarea} = useState()
+  const { tareas, setTareas } = useContext(Contexto);
+  const [tarea, setTarea] = useState("");
 
-    const handleGuardado = () => {
-
+  const handleGuardado = () => {
+    if (tarea !== "") {
+      setTareas([...tareas, tarea]);
+      setTarea("");
+    } else {
+      alert("Debe ingresar una tarea para guardarla");
     }
-    return (
+  };
+
+  const handleEliminar = (index) => {
+    const listadoModificado = tareas.filter(( tarea,i) => i !== index);
+    setTareas(listadoModificado);
+  };
+
+  return (
+    <div className='caja'>
+      <div>
+        <h2>Lista de tareas</h2>
+        <input
+          type="text"
+          placeholder='Ingrese una tarea'
+          value={tarea}
+          onChange={(evento) => setTarea(evento.target.value)}
+        />
+        <button onClick={handleGuardado}>Guardar</button>
+      </div>
+      <div>
+        <h4>Lista de tareas</h4>
         <div className='caja'>
-            <div>
-                <h2>Lista de tareas</h2>
-                <input type="text" placeholder='Ingrese una tarea'  value={tarea} onChange={(evento)=> setTarea(evento.target.value)}/>
-            </div>
-            <div>
-                <h4>Lista de tareas</h4>
-                <div className='caja'>
-                    <div>
-                        <li>una tarea</li>
-                        <button>X</button>
-                    </div>
-                </div>
-            </div>
+          <div>
+            {tareas.map((tarea, index) => (
+              <div key={index}>
+                <li>{tarea}</li>
+                <button onClick={() => handleEliminar(index)}>X</button>
+              </div>
+            ))}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
